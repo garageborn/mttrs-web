a2d1d716e64e35f21561bc4a69c995eccda70da8
Alexandre Bini <alexandrebini@gmail.com>
2016-12-08 16:14:48 -0200

    Remove Native (#1)

--- index.android.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { AppRegistry } from 'react-native'
-import Root from './app/native/containers/Root'
-import configureStore from './app/native/config/configureStore'
-
-const store = configureStore()
-const mttrs = () => <Root store={store}/>
-AppRegistry.registerComponent('mttrs', () => mttrs)
