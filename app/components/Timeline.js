8347d9178f8d5b87a6ba015e6ca0f390877ec12e
Klaus <klausy@gmail.com>
2016-12-13 12:12:25 +0000

    New translations

--- app/components/Timeline.js
+++ app/components/Timeline.js
@@ -1,23 +1,13 @@
 import React, {Component, PropTypes} from 'react'
 import StoryList from './StoryList'
-import { injectIntl, defineMessages } from 'react-intl'
-const messages = defineMessages({
-  headerTitle: {
-    id: 'header.title',
-    defaultMessage: 'Top Stories'
-  }
-})
 
 class Timeline extends Component {
   render () {
     const {items, isFetching} = this.props
     if (isFetching) return (<div className='loading'>Hang on...</div>)
-    const { formatMessage } = this.props.intl
-    let title = formatMessage(messages.headerTitle)
 
     return (
       <main>
-        <h1>{title}</h1>
         {items.map((item) => {
           return <StoryList key={item.date} date={item.date} stories={item.stories} />
         })}
@@ -31,4 +21,4 @@ Timeline.propTypes = {
   isFetching: PropTypes.bool.isRequired
 }
 
-export default injectIntl(Timeline)
+export default Timeline
