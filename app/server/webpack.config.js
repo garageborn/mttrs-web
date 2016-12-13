4881111ee366ad32efc099d810de38e6096829f5
AgtLucas <lucas@agtlucas.com>
2016-12-12 22:17:48 -0200

    Update webpack.config.js

--- app/server/webpack.config.js
+++ app/server/webpack.config.js
@@ -1,5 +1,9 @@
 require('babel-core/register')
 
+require.extensions['.css'] = () => {
+  return
+}
+
 const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
