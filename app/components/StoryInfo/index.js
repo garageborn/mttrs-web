7d200c86ed37649a52d4a80790ae89fd204e1c18
Klaus <klausy@gmail.com>
2016-12-15 17:28:43 +0000

    Fix styles on publisher related metadata

--- app/components/StoryInfo/index.js
+++ app/components/StoryInfo/index.js
@@ -1,20 +1,34 @@
 import React, { Component, PropTypes } from 'react'
-import PublisherTag from '../PublisherTag'
-import {publisherPath} from '../../utils/RoutesHelper'
-import ParseDate from '../../common/utils/ParseDate'
-import moment from '../../common/utils/Moment'
+import PublisherIcon from '../PublisherIcon'
+import styles from './styles.css'
 
 class StoryMetadata extends Component {
-  renderPublishers() {
-    if (this.props.otherLinks.length === 0) return this.props.mainLink.publisher.name
-    if (this.props.otherLinks.length === 1) return `${this.props.mainLink.publisher.name} and 1 other`
-    return `${this.props.mainLink.publisher.name} and ${this.props.otherLinks.length} others`
+  renderPublishers () {
+    if (this.props.otherLinks.length === 0) return this.renderMainPublisherName()
+    return this.renderPublishersText()
   }
-  render() {
+
+  renderPublishersText () {
+    return <p>{this.renderMainPublisherName()} and {this.renderOtherLinks()}</p>
+  }
+
+  renderMainPublisherName () {
+    return <a href='#'>{this.props.mainLink.publisher.name}</a>
+  }
+
+  renderOtherLinks () {
+    let otherString = 'other'
+    if (this.props.otherLinks.length > 1) otherString = 'others'
+    return <a href='#'>{this.props.otherLinks.length} {otherString}</a>
+  }
+
+  renderPublisherIcon () {
+    return <PublisherIcon source='storyInfo' publisher={this.props.mainLink.publisher} />
+  }
+
+  render () {
     return (
-      <div>
-        @{ParseDate(moment(this.props.mainLink.published_at).unix())} from {this.renderPublishers()}
-      </div>
+      <div className={styles.text}>From&nbsp;{this.renderPublisherIcon()}&nbsp;{this.renderPublishers()}</div>
     )
   }
 }
