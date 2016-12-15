ca431e0dd475517e361bab503d6a0ff2a8463061
Klaus <klausy@gmail.com>
2016-12-15 10:19:37 +0000

    Fix story images

--- app/components/StoryImage/index.js
+++ app/components/StoryImage/index.js
@@ -1,29 +1,30 @@
 import React, { Component, PropTypes } from 'react'
 import * as cloudinary from '../../common/utils/Cloudinary'
-import ParseDate from '../../common/utils/ParseDate'
 import styles from './styles.css'
 
 class StoryImage extends Component {
-  renderImage() {
+  renderImage () {
     if (!this.props.mainLink.image_source_url) return
     let options = {
-      type: 'fetch',
-      width: 200,
-      height: 200,
-      crop: 'fit',
-      secure: true
+      type: 'fetch', width: 120, height: 90, crop: 'fill', secure: true
     }
     return (
-      <img
-        className={styles.image}
-        alt={this.props.mainLink.title}
-        src={cloudinary.url(this.props.mainLink.image_source_url, options)}
-      />
+      <a href={this.props.mainLink.url} target='_blank'>
+        <img
+          className={styles.image}
+          alt={this.props.mainLink.title}
+          src={cloudinary.url(this.props.mainLink.image_source_url, options)}
+        />
+      </a>
     )
   }
 
-  render() {
-    return <a href={this.props.mainLink.url} target='_blank'>{this.renderImage()}</a>
+  render () {
+    return (
+      <aside className={styles.container}>
+        {this.renderImage()}
+      </aside>
+    )
   }
 }
 
