eaa72e283d175acf4447c265b8d353c220eae360
Alexandre Bini <alexandrebini@gmail.com>
2016-12-14 23:24:20 -0200

    Update Root.js

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import HeaderContainer from './HeaderContainer'
 import TimelineContainer from './TimelineContainer'
 import IntlProvider from '../config/IntlProvider'
