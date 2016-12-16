d13f2b233065824b3d44ffedd90fec30156f754c
AgtLucas <lucas@agtlucas.com>
2016-12-16 15:21:35 -0200

    Update timeline

--- app/components/StoryListHeader/index.js
+++ app/components/StoryListHeader/index.js
@@ -1,8 +1,9 @@
 import React, { PropTypes } from 'react'
 import ParseDate from '../../common/utils/ParseDate'
+import styles from './styles.css'
 
 const StoryListHeader = ({date}) => {
-  return <h2>{ParseDate(date)}</h2>
+  return <h2 className={styles.title}>{ParseDate(date)}</h2>
 }
 
 StoryListHeader.propTypes = {
