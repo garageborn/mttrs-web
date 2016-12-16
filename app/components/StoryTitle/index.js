a6a029f338aed955beea7de23604ad3919965d5a
Klaus <klausy@gmail.com>
2016-12-16 10:51:58 +0000

    Add line clamp

--- app/components/StoryTitle/index.js
+++ app/components/StoryTitle/index.js
@@ -1,11 +1,14 @@
 import React, { PropTypes } from 'react'
 import styles from './styles.css'
+import Shiitake from 'shiitake'
 
 const StoryTitle = ({mainLink}) => {
   return (
-    <h3 className={styles.title}>
-      <a href={mainLink.url} target='_blank'>{mainLink.title}</a>
-    </h3>
+    <a href={mainLink.url} target='_blank'>
+      <Shiitake lines={3} className={styles.title} throttleRate={200} tagName='h3'>
+        {mainLink.title}
+      </Shiitake>
+    </a>
   )
 }
 
