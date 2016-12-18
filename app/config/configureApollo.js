cecada4e39bd7e642059d113b7c686710eca9b58
Alexandre Bini <alexandrebini@gmail.com>
2016-12-18 21:07:44 -0200

    tabs

--- app/config/configureApollo.js
+++ app/config/configureApollo.js
@@ -1,12 +1,10 @@
 import ApolloClient, { createNetworkInterface } from 'apollo-client'
-// import * as ENDPOINTS from '../constants/APIEndpoints'
+import * as ENDPOINTS from '../constants/APIEndpoints'
 // import Tenant from '../common/utils/Tenant'
 
 export default function configureApollo (options = {}) {
   const networkInterface = createNetworkInterface(
-    // Object.assign({}, { uri: ENDPOINTS.GRAPHQL }, options)
-    Object.assign({}, { uri: 'https://api.mtt.rs/graphql' }, options)
-    // Object.assign({}, { uri: 'http://localhost:4000/graphql' }, options)
+    Object.assign({}, { uri: ENDPOINTS.GRAPHQL }, options)
   )
   //
   // networkInterface.use([{
