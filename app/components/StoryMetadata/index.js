4cba89e9552aaf2a803044768c3fd00ab6df5640
Lucas <AgtLucas@users.noreply.github.com>
2016-12-19 16:04:13 -0200

    Update index.js

--- app/components/StoryMetadata/index.js
+++ app/components/StoryMetadata/index.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react'
-import classNames from 'classNames'
+import classNames from 'classnames'
 import StoryInfo from '../StoryInfo'
 import SocialCount from '../SocialCount'
 import styles from './styles.css'
