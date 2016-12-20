e7db9585f493b01417c7d676a2a4df41f0983079
Alexandre Bini <alexandrebini@gmail.com>
2016-12-20 12:06:36 -0200

    classNames != classnames

--- app/components/PublisherIcon/index.js
+++ app/components/PublisherIcon/index.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react'
-import classNames from 'classNames'
+import classNames from 'classnames'
 import * as cloudinary from '../../common/utils/Cloudinary'
 import styles from './styles.css'
 
--- app/components/StoryLink/index.js
+++ app/components/StoryLink/index.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react'
-import classNames from 'classNames'
+import classNames from 'classnames'
 import { Link } from 'react-router'
 import PublisherIcon from '../PublisherIcon'
 import SocialCount from '../SocialCount'
