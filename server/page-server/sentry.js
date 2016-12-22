4e95ce3a7a47393fa24cb2835594dc021d23cf4c
Alexandre Bini <alexandrebini@gmail.com>
2016-12-22 12:05:54 -0200

    sentry dsn

--- server/page-server/sentry.js
+++ server/page-server/sentry.js
@@ -1,5 +1,5 @@
 import raven from 'raven'
-const sentry = new raven.Client(process.env.MTTRS_FRONTEND_SENTRY_DSN)
+const sentry = new raven.Client(process.env.MTTRS_WEB_SENTRY_DSN)
 
 if (process.env.NODE_ENV === 'production') sentry.patchGlobal()
 
