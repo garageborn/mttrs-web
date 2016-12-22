f650dbff33d644835f61629e555caa5ea81491b4
AgtLucas <lucas@agtlucas.com>
2016-12-22 11:51:20 -0200

    Update menu

--- app/components/MenuCategories/index.js
+++ app/components/MenuCategories/index.js
@@ -1,11 +1,19 @@
 import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
 import styles from './styles.css'
 import MenuCategoriesItem from '../MenuCategoriesItem'
 
 const MenuCategories = ({categories}) => {
   return (
     <div className={styles.container}>
-      {categories.map((category) => <MenuCategoriesItem key={category.id} category={category} />)}
+      <Link to='/'>
+        <div>
+          <p>Top Stories</p>
+        </div>
+      </Link>
+      <div className={styles.categories}>
+        {categories.map((category) => <MenuCategoriesItem key={category.id} category={category} />)}
+      </div>
     </div>
   )
 }
--- app/components/MenuCategoriesItem/index.js
+++ app/components/MenuCategoriesItem/index.js
@@ -1,12 +1,32 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { Link } from 'react-router'
+import { categoryPath } from '../../utils/RoutesHelper'
+import * as cloudinary from '../../common/utils/Cloudinary'
 import styles from './styles.css'
 
-const MenuCategoriesItem = ({category}) => {
-  return (
-    <div className={styles.container}>
-      {category.name}
-    </div>
-  )
+class MenuCategoriesItem extends Component {
+  render () {
+    const { category } = this.props
+    return (
+      <Link to={categoryPath(category.slug)} className={styles.category}>
+        <div className={styles.container}>
+          <img className={styles.icon} src={this.getIcon()} alt='' />
+          <p className={styles.title} style={{color: category.color}}>{category.name}</p>
+        </div>
+      </Link>
+    )
+  }
+
+  getIcon () {
+    const { category } = this.props
+    if (!category.icon_id) return
+
+    let options = {
+      secure: true
+    }
+
+    return cloudinary.id(category.icon_id, options)
+  }
 }
 
 MenuCategoriesItem.propTypes = {
