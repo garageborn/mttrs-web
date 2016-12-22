5cbd9abc5f227c91bac30d61f18884fb60033aba
Lucas <AgtLucas@users.noreply.github.com>
2016-12-21 23:18:41 -0200

    Add more translations (#39)

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,35 +1,18 @@
-import React, { Component, PropTypes } from 'react'
-import { injectIntl, defineMessages } from 'react-intl'
+import React, { PropTypes } from 'react'
 import SubHeader from '../SubHeader'
 import NavContainer from '../../containers/NavContainer'
 import { rootPath } from '../../utils/RoutesHelper'
 import styles from './styles.css'
 
-const messages = defineMessages({
-  headerTagline: {
-    id: 'header.tagline',
-    defaultMessage: 'Read What Matters'
-  }
-})
-
-class Header extends Component {
-  render () {
-    const { formatMessage } = this.props.intl
-    let headerTagline = formatMessage(messages.headerTagline)
-
-    return (
-      <header className={styles.header}>
-        <div>
-          <SubHeader rootPath={rootPath} />
-          <NavContainer />
-        </div>
-      </header>
-    )
-  }
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div>
+        <SubHeader rootPath={rootPath} />
+        <NavContainer />
+      </div>
+    </header>
+  )
 }
 
-Header.propTypes = {
-  intl: PropTypes.object.isRequired
-}
-
-export default injectIntl(Header)
+export default Header
--- app/components/SubHeader/index.js
+++ app/components/SubHeader/index.js
@@ -1,28 +1,47 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
+import { injectIntl, defineMessages } from 'react-intl'
 import Logo from '../Logo'
 import Arrow from '../Arrow'
 import styles from './styles.css'
 
-const SubHeader = (props) => {
-  return (
-    <section className={styles.subHeader}>
-      <div className={styles.logoSection}>
-        <Link to={props.rootPath} className={styles.title}>
-          <Logo />
-        </Link>
-      </div>
-      <div className={styles.menuTrigger}>
-        <Logo type='mobile' />
-        <span className={styles.title}>Top Stories</span>
-        <Arrow />
-      </div>
-    </section>
-  )
+const messages = defineMessages({
+  topStories: {
+    id: 'header.topStories',
+    defaultMessage: 'Top Stories'
+  }
+})
+
+class SubHeader extends Component {
+  render () {
+    const { rootPath } = this.props
+    return (
+      <section className={styles.subHeader}>
+        <div className={styles.logoSection}>
+          <Link to={rootPath} className={styles.title}>
+            <Logo />
+          </Link>
+        </div>
+        <div className={styles.menuTrigger}>
+          <Logo type='mobile' />
+          <span className={styles.title}>{this.getTitle()}</span>
+          <Arrow />
+        </div>
+      </section>
+    )
+  }
+
+  getTitle () {
+    const { formatMessage } = this.props.intl
+    return formatMessage(messages.topStories)
+  }
 }
 
 SubHeader.propTypes = {
-  rootPath: PropTypes.string.isRequired
+  rootPath: PropTypes.string.isRequired,
+  intl: PropTypes.shape({
+    formatMessage: PropTypes.func.isRequired
+  }).isRequired
 }
 
-export default SubHeader
+export default injectIntl(SubHeader)
