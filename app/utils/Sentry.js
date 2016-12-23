5f8fff9629e7d0e0f35bda2f3dd1d3a72f2c175e
Alexandre Bini <alexandrebini@gmail.com>
2016-12-23 12:47:41 -0200

    remove sentry from development

--- app/utils/Sentry.js
+++ app/utils/Sentry.js
@@ -1,11 +1,14 @@
-export default [
-  {
-    type: 'text/javascript',
-    src: 'https://cdn.ravenjs.com/3.9.1/raven.min.js',
-    crossOrigin: 'anonymous'
-  },
-  {
-    type: 'text/javascript',
-    innerHTML: "Raven.config('https://b2086d08c0144a8cac94f13d30a578f1@sentry.io/124314').install()"
-  }
-]
+export default (function () {
+  if (_development_) return []
+  return [
+    {
+      type: 'text/javascript',
+      src: 'https://cdn.ravenjs.com/3.9.1/raven.min.js',
+      crossOrigin: 'anonymous'
+    },
+    {
+      type: 'text/javascript',
+      innerHTML: "Raven.config('https://b2086d08c0144a8cac94f13d30a578f1@sentry.io/124314').install()"
+    }
+  ]
+})()
