92027f176a431f9f7d4f8607a7e3d9abf6477d72
Lucas <AgtLucas@users.noreply.github.com>
2016-12-23 08:01:36 -0200

    Update publisher menu (#46)

--- app/components/MenuPublishers/index.js
+++ app/components/MenuPublishers/index.js
@@ -1,18 +1,40 @@
-import React, { PropTypes } from 'react'
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component, PropTypes } from 'react'
 import MenuPublishersItem from '../MenuPublishersItem'
+import { injectIntl, defineMessages } from 'react-intl'
+import Loupe from '../../assets/loupe.svg'
 import styles from './styles.css'
 
-const MenuPublishers = ({publishers}) => {
-  return (
-    <div className={styles.container}>
-      {publishers.map((publisher) => <MenuPublishersItem key={publisher.id} publisher={publisher} />)}
-    </div>
+const messages = defineMessages({
+  searchPublishers: {
+    id: 'publishers.search',
+    defaultMessage: 'Filter publishers'
+  }
+})
 
-  )
+class MenuPublishers extends Component {
+  render () {
+    const { publishers } = this.props
+    const { formatMessage } = this.props.intl
+    return (
+      <div className={styles.container}>
+        <div className={styles.search}>
+          <Loupe />
+          <input className={styles.input} placeholder={formatMessage(messages.searchPublishers)} />
+        </div>
+        <div className={styles.publishers}>
+          {publishers.map((publisher) => <MenuPublishersItem key={publisher.id} publisher={publisher} />)}
+        </div>
+      </div>
+    )
+  }
 }
 
 MenuPublishers.propTypes = {
-  publishers: PropTypes.array.isRequired
+  publishers: PropTypes.array.isRequired,
+  intl: PropTypes.shape({
+    formatMessage: PropTypes.func.isRequired
+  }).isRequired
 }
 
-export default MenuPublishers
+export default injectIntl(MenuPublishers)
