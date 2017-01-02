6c6a596bfd5f8ffa4ffa268c09efc90f57d8663b
AgtLucas <lucas@agtlucas.com>
2017-01-02 08:24:00 -0200

    i18n

--- app/components/MenuSelector/index.js
+++ app/components/MenuSelector/index.js
@@ -1,19 +1,38 @@
-/* eslint-disable react/jsx-no-bind */
-import React, { PropTypes } from 'react'
+/* eslint-disable react/jsx-no-bind, react/prefer-stateless-function */
+import React, { Component, PropTypes } from 'react'
+import { injectIntl, defineMessages } from 'react-intl'
 import styles from './styles.css'
 
-const MenuSelector = ({activePanel, selectPanel}) => {
-  return (
-    <div className={styles.container}>
-      <button className={styles.button} onClick={() => selectPanel('categories')}>Categories</button>
-      <button className={styles.button} onClick={() => selectPanel('publishers')}>Publishers</button>
-    </div>
-  )
+const messages = defineMessages({
+  categories: {
+    id: 'selector.categories',
+    defaultMessage: 'Categories'
+  },
+
+  publishers: {
+    id: 'selector.publishers',
+    defaultMessage: 'Publishers'
+  }
+})
+
+class MenuSelector extends Component {
+  render () {
+    const { selectPanel } = this.props
+    const { formatMessage } = this.props.intl
+    return (
+      <div className={styles.container}>
+        <button className={styles.button} onClick={() => selectPanel('categories')}>{formatMessage(messages.categories)}</button>
+        <button className={styles.button} onClick={() => selectPanel('publishers')}>{formatMessage(messages.publishers)}</button>
+      </div>
+    )
+  }
 }
 
 MenuSelector.propTypes = {
-  activePanel: PropTypes.string.isRequired,
-  selectPanel: PropTypes.func.isRequired
+  selectPanel: PropTypes.func.isRequired,
+  intl: PropTypes.shape({
+    formatMessage: PropTypes.func.isRequired
+  }).isRequired
 }
 
-export default MenuSelector
+export default injectIntl(MenuSelector)
