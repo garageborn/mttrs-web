2e8ac5aa5236fd9edaff99c9fa165defcbd2f63f
AgtLucas <lucas@agtlucas.com>
2017-01-04 11:07:23 -0200

    Add image loading

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -59,7 +59,7 @@ var configuration = {
         ]
       },
       {
-        test: /\.(jpg|png|ico)$/,
+        test: /\.(jpg|png|ico|gif)$/,
         loaders:
         [
           'url-loader?limit=1' // Any png-image or woff-font below or equal to 10K will be converted to inline base64 instead
