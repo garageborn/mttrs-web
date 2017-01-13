354a2ea777a45dfc255844ed21d9a1155d5a4352
Klaus Kohut <klausy@gmail.com>
2017-01-13 12:35:04 +0000

    Redesign categories on cards

--- app/components/StoryContent/index.js
+++ app/components/StoryContent/index.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react'
 import StoryTitle from '../StoryTitle'
 import styles from './styles.css'
+import StoryCategory from '../StoryCategory'
 
 const StoryContent = ({
   story,
@@ -8,11 +9,18 @@ const StoryContent = ({
   otherLinks,
   totalSocial,
   handleStoryLinks,
-  handleVisitedStory
+  handleVisitedStory,
+  shouldRenderCategory,
+  category
 }) => {
+  const renderCategory = () => {
+    if (shouldRenderCategory === false) return
+    return <StoryCategory category={category} />
+  }
   return (
     <div className={styles.container}>
       <StoryTitle mainLink={mainLink} handleVisitedStory={handleVisitedStory} />
+      {renderCategory()}
     </div>
   )
 }
@@ -23,7 +31,9 @@ StoryContent.propTypes = {
   otherLinks: PropTypes.array,
   totalSocial: PropTypes.number,
   handleStoryLinks: PropTypes.func.isRequired,
-  handleVisitedStory: PropTypes.func.isRequired
+  handleVisitedStory: PropTypes.func.isRequired,
+  shouldRenderCategory: PropTypes.bool.isRequired,
+  category: PropTypes.object.isRequired
 }
 
 export default StoryContent
