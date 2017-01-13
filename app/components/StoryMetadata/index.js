0331dc4f54ea64051b0957afcda74c35ba1b4332
Klaus <klausy@gmail.com>
2017-01-13 13:46:25 +0000

    Features/history card redesign (#55)
    * Remove story metadata from story container
    
    * Fix typography in story title
    
    * Fix image margins
    
    * Fix StoryTitle margin
    
    * Add StoryMetadata styles, improve story card styles
    
    * Redesign categories on cards
    
    * Remove comments

--- app/components/StoryMetadata/index.js
+++ app/components/StoryMetadata/index.js
@@ -1,23 +1,14 @@
 import React, { Component, PropTypes } from 'react'
-import classNames from 'classnames'
 import StoryInfo from '../StoryInfo'
 import SocialCount from '../SocialCount'
 import styles from './styles.css'
 
 class StoryMetadata extends Component {
-  getClassNames () {
-    return classNames({
-      [styles.container]: true,
-      [styles.shouldShowNotSmall]: this.props.source === 'storyContent',
-      [styles.shouldShowSmall]: this.props.source === 'story'
-    })
-  }
-
   render () {
     let {story, mainLink, otherLinks, totalSocial, handleStoryLinks} = this.props
 
     return (
-      <div className={this.getClassNames()}>
+      <div className={styles.container}>
         <StoryInfo
           story={story}
           mainLink={mainLink}
@@ -35,7 +26,6 @@ StoryMetadata.propTypes = {
   mainLink: PropTypes.object.isRequired,
   otherLinks: PropTypes.array.isRequired,
   totalSocial: PropTypes.number.isRequired,
-  source: PropTypes.string.isRequired,
   handleStoryLinks: PropTypes.func.isRequired
 }
 
