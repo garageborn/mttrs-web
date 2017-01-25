a7debb46c306ee35bf44aa5d816746e7d9fc3817
Alexandre Bini <alexandrebini@gmail.com>
2017-01-25 14:05:09 -0200

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,2 @@
-server 'app01.server.garageborn.com', user: 'garageborn', roles: %w{app}
+# server 'app01.server.garageborn.com', user: 'garageborn', roles: %w{app}
+server 'app02.server.garageborn.com', user: 'garageborn', roles: %w{app}
