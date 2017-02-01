4e3c427a849d65a77fa58c25d6bd9652daf580e9
Alexandre Bini <alexandrebini@gmail.com>
2017-02-01 16:59:03 -0200

    custom network interface - jesus

--- app/constants/APIEndpoints.js
+++ app/constants/APIEndpoints.js
@@ -1,4 +1,2 @@
 const ROOT = process.env.MTTRS_API_URL || 'https://api.mtt.rs'
-// const ROOT = process.env.MTTRS_API_URL || 'http://localhost:3000'
-// const ROOT = 'http://d22598ll8icl5v.cloudfront.net'
 export const GRAPHQL = `${ROOT}/graphql`
