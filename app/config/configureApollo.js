483d4fa88b5d1c09a5a00523ccbc711dc7983c39
Klaus Kohut <klausy@gmail.com>
2017-03-06 12:12:11 +0000

    Revert hardcoded tenant

--- app/config/configureApollo.js
+++ app/config/configureApollo.js
@@ -6,7 +6,7 @@ const tenantMiddleware = (options) => {
   return {
     applyMiddleware: (req, next) => {
       if (!req.options.headers) req.options.headers = {}
-      req.options.headers['X-Tenant'] = 'mttrs_br'
+      req.options.headers['X-Tenant'] = options.tenant
       next()
     }
   }
