71216b00c14c8827e74509b59f1665b9e36cc737
Klaus Kohut <klausy@gmail.com>
2017-03-06 11:28:08 +0000

    Add placeholders

--- app/components/StoryImage/index.js
+++ app/components/StoryImage/index.js
@@ -1,40 +1,63 @@
 import React, { Component, PropTypes } from 'react'
 import { Link } from 'react-router'
-import { linkPath } from '../../utils/RoutesHelper'
 import LazyLoad from 'react-lazy-load'
 import * as cloudinary from '../../utils/Cloudinary'
+import { linkPath } from '../../utils/RoutesHelper'
+import Placeholder from './components/Placeholder'
 import styles from './styles.css'
 
 class StoryImage extends Component {
-  renderImage () {
-    const { mainLink } = this.props
-    if (!this.mainLink.image_source_url) return
+  constructor () {
+    super()
+    this.handleImageError = this.handleImageError.bind(this)
+    this.state = { status: 'loading' }
+  }
+
+  componentWillMount () {
+    if (!this.mainLink.image_source_url) this.setState({ status: 'error' })
+  }
+
+  getSource () {
+    if (this.state.status === 'error') return
+    return this.getImage()
+  }
+
+  getImage () {
     let options = {
       type: 'fetch', width: 120, height: 90, crop: 'fill', secure: true
     }
+    return cloudinary.url(this.mainLink.image_source_url, options)
+  }
 
-    return (
-      <Link
-        to={linkPath(mainLink.slug)}
-        className={styles.link}
-        target='_blank'
-      >
-        <LazyLoad height={90} width={120} offset={300}>
-          <img
-            className={styles.image}
-            alt={this.mainLink.title}
-            src={cloudinary.url(this.mainLink.image_source_url, options)}
-          />
-        </LazyLoad>
-      </Link>
-    )
+  handleImageError () {
+    this.setState({ status: 'error' })
   }
 
   render () {
     return (
       <aside className={styles.container}>
-        {this.renderImage()}
+        {this.renderImage}
       </aside>
+    )
+  }
+
+  get renderImage () {
+    if (this.state.status === 'error') return <Placeholder story={this.props.story} />
+    return (
+      <Link
+        to={linkPath(this.mainLink.slug)}
+        className={styles.link}
+        target='_blank'
+      >
+        <LazyLoad height={90} width={120} offset={300}>
+          <img
+            onError={this.handleImageError}
+            className={styles.image}
+            alt={this.mainLink.title}
+            src={this.getSource()}
+          />
+        </LazyLoad>
+      </Link>
     )
   }
 
