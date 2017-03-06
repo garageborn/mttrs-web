fea1fff19c6611e977ee2ea52b63dc5effd355f1
Klaus <klausy@gmail.com>
2017-03-06 12:24:22 +0000

    Add placeholders (#80)
    * Add placeholders
    
    * Remove retina placeholders
    
    * Remove faulty file
    
    * Revert hardcoded tenant
    
    * Lint

---
+++ app/components/StoryImage/components/Placeholder/index.js
@@ -0,0 +1,35 @@
+import React, { Component, PropTypes } from 'react'
+import styles from '../../styles.css'
+
+const firstPlaceholder = require('./assets/placeholder-01.png')
+const secondPlaceholder = require('./assets/placeholder-02.png')
+const thirdPlaceholder = require('./assets/placeholder-03.png')
+const fourthPlaceholder = require('./assets/placeholder-04.png')
+const fifthPlaceholder = require('./assets/placeholder-05.png')
+
+const placeholders = [
+  firstPlaceholder, firstPlaceholder,
+  secondPlaceholder, secondPlaceholder,
+  thirdPlaceholder, thirdPlaceholder,
+  fourthPlaceholder, fourthPlaceholder,
+  fifthPlaceholder, fifthPlaceholder
+]
+
+class Placeholder extends Component {
+  render () {
+    return <img className={styles.image} src={this.getPlaceholder()} />
+  }
+
+  getPlaceholder () {
+    const lastDigit = this.props.story.id % 10
+    return placeholders[lastDigit]
+  }
+}
+
+Placeholder.propTypes = {
+  story: PropTypes.shape({
+    id: PropTypes.any.isRequired
+  }).isRequired
+}
+
+export default Placeholder
