7684f04893fb702e1ee9f78d2fe6ffc3d43b036f
AgtLucas <lucas@agtlucas.com>
2017-03-09 08:42:25 -0300

    Lock menu on desktop

--- app/containers/MenuContainer.js
+++ app/containers/MenuContainer.js
@@ -1,7 +1,9 @@
 import React, { Component, PropTypes } from 'react'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import MediaQuery from 'react-responsive'
 import { connect } from 'react-redux'
 import { UIActions } from '../actions/index'
+import { mediaQueries } from '../utils/variables'
 import Menu from '../components/Menu'
 import menuAnimations from '../styles/menu-animations.css'
 
@@ -13,9 +15,14 @@ class MenuContainer extends Component {
 
   render () {
     return (
-      <ReactCSSTransitionGroup transitionName={menuAnimations} transitionAppear transitionEnter transitionAppearTimeout={330} transitionEnterTimeout={330} transitionLeaveTimeout={330}>
-        {this.renderMenu()}
-      </ReactCSSTransitionGroup>
+      <div>
+        <ReactCSSTransitionGroup transitionName={menuAnimations} transitionAppear transitionEnter transitionAppearTimeout={330} transitionEnterTimeout={330} transitionLeaveTimeout={330}>
+          {this.renderMenu()}
+        </ReactCSSTransitionGroup>
+        <MediaQuery query={mediaQueries.large} minDeviceWidth={1140}>
+          <Menu closeMenu={this.closeMenu} />
+        </MediaQuery>
+      </div>
     )
   }
 
@@ -23,7 +30,7 @@ class MenuContainer extends Component {
     const { UIReducer } = this.props
     if (!UIReducer.menu.isOpen) return null
     return (
-      <Menu key='mobileMenu' closeMenu={this.closeMenu} isOpen={UIReducer.menu.isOpen} />
+      <Menu closeMenu={this.closeMenu} isOpen={UIReducer.menu.isOpen} />
     )
   }
 
