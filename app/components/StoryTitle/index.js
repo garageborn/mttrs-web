a9d102ff27789af75688d8487ff92aa63459dc8a
Lucas <AgtLucas@users.noreply.github.com>
2017-03-20 16:24:55 -0300

    Improve summary styles (#106)

--- app/components/StoryTitle/index.js
+++ app/components/StoryTitle/index.js
@@ -1,14 +1,15 @@
 /* eslint-disable react/jsx-no-bind */
 import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
-import { linkPath } from '../../utils/RoutesHelper'
 import classNames from 'classnames'
+import { linkPath } from '../../utils/RoutesHelper'
 import styles from './styles.css'
 
-const StoryTitle = ({mainLink, handleMouseOver, active}) => {
+const StoryTitle = ({ mainLink, handleMouseOver, active, isVisited }) => {
   let textClass = classNames({
     [styles.title]: true,
-    [styles.active]: active
+    [styles.active]: active,
+    [styles.isVisited]: isVisited
   })
 
   return (
@@ -27,7 +28,8 @@ const StoryTitle = ({mainLink, handleMouseOver, active}) => {
 StoryTitle.propTypes = {
   mainLink: PropTypes.object.isRequired,
   handleMouseOver: PropTypes.func.isRequired,
-  active: PropTypes.bool.isRequired
+  active: PropTypes.bool.isRequired,
+  isVisited: PropTypes.bool.isRequired
 }
 
 export default StoryTitle
