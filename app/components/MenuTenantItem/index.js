bbd268f84d67cd2e0e9058b48ab7a6a2ee17cf8e
Klaus <klausy@gmail.com>
2017-03-27 14:52:02 +0100

    Add language selection to menu (#110)
    * Add language selection to menu
    
    * Fix urls

---
+++ app/components/MenuTenantItem/index.js
@@ -0,0 +1,37 @@
+import React, { PropTypes } from 'react'
+import styles from './styles.css'
+import image from './assets/image.svg'
+
+let urls = {
+  mttrs_br: 'https://mttrs.com.br',
+  mttrs_us: 'https://mtt.rs'
+}
+
+const MenuTenantItem = ({ tenant, currentTenant, children }) => {
+  const renderText = () => {
+    if (tenant === currentTenant) {
+      return (
+        <p className={styles.text}>
+          {children}
+          <img className={styles.image} src={image} alt='' />
+        </p>
+      )
+    } else {
+      return <a href={urls[tenant]} className={styles.link}>{children}</a>
+    }
+  }
+
+  return (
+    <li className={styles.container}>
+      {renderText()}
+    </li>
+  )
+}
+
+MenuTenantItem.propTypes = {
+  tenant: PropTypes.string.isRequired,
+  currentTenant: PropTypes.string.isRequired,
+  children: PropTypes.isRequired
+}
+
+export default MenuTenantItem
