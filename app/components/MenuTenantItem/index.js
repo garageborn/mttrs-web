33f3849ffba86160c1b1623070b268a554d065d8
Alexandre Bini <alexandrebini@gmail.com>
2017-06-22 12:44:51 -0300

    new tenants

--- app/components/MenuTenantItem/index.js
+++ app/components/MenuTenantItem/index.js
@@ -4,7 +4,10 @@ import image from './assets/image.svg'
 
 let urls = {
   mttrs_br: 'https://mttrs.com.br',
-  mttrs_us: 'https://mtt.rs'
+  mttrs_us: 'https://mtt.rs',
+  mttrs_ar: 'https://mtt.rs',
+  mttrs_cl: 'https://mtt.rs',
+  mttrs_mx: 'https://mtt.rs',
 }
 
 const MenuTenantItem = ({ tenant, currentTenant, children }) => {
