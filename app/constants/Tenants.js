6a61d59c720d68d4b4823abddc7fc0b21a11bd0a
Alexandre Bini <alexandrebini@gmail.com>
2017-06-22 16:29:04 -0300

    origin domains

--- app/constants/Tenants.js
+++ app/constants/Tenants.js
@@ -1,7 +1,7 @@
 export const TENANTS = [
-  { id: 'mttrs_br', domain: /mttrs\.com\.br/ },
-  { id: 'mttrs_ar', domain: /ar\.mttrs\.rs/ },
-  { id: 'mttrs_cl', domain: /cl\.mttrs\.rs/ },
-  { id: 'mttrs_mx', domain: /mx\.mttrs\.rs/ },
-  { id: 'mttrs_us', domain: /mtt\.rs/ } // always in the end
+  { id: 'mttrs_br', domain: /(mttrs|origin\.mttrs)\.com\.br/ },
+  { id: 'mttrs_ar', domain: /(ar|ar\.origin)\.mtt\.rs/ },
+  { id: 'mttrs_cl', domain: /(cl|cl\.origin)\.mtt\.rs/ },
+  { id: 'mttrs_mx', domain: /(mx|mx\.origin)\.mtt\.rs/ },
+  { id: 'mttrs_us', domain: /(mtt|origin\.mtt)\.rs/ } // always in the end
 ]
