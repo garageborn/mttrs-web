f1e1ad2cb58316c3d5bace56bb6a0ca7f20e15f1
Alexandre Bini <alexandrebini@gmail.com>
2017-06-23 12:12:12 -0300

    persistent assets

--- webpack/universal-webpack-settings.js
+++ webpack/universal-webpack-settings.js
@@ -1,33 +1,6 @@
-import path from 'path'
-
-// project folder
-const Root = path.resolve(__dirname, '..')
-
 module.exports = {
-  // resolve all relative paths from the project root folder
-  context: Root,
-
-  // https://webpack.github.io/docs/multiple-entry-points.html
-  entry: {
-    main: './server/page-server/webserver.js'
-  },
-
-  output: {
-    // filesystem path for static files
-    path: path.resolve(Root, 'build/server'),
-
-    // file name pattern for entry scripts
-    filename: '[name].[hash].js',
-
-    // file name pattern for chunk scripts
-    chunkFilename: '[name].[hash].js'
+  server: {
+    input: './server/page-server/webserver.js',
+    output: './build/server/server.js'
   }
 }
-
-// module.exports = {
-//   server: {
-//     input: './server/page-server/webserver.js',
-
-//     output: './build/server/server.js'
-//   }
-// }
