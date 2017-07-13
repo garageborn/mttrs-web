99b2976a85dbfaef25b98ee62221cf26082c81d8
Alexandre Bini <alexandrebini@gmail.com>
2017-07-13 11:17:55 -0300

    canada/germany/spain/uk

--- app/constants/Tenants.js
+++ app/constants/Tenants.js
@@ -6,5 +6,9 @@ export const TENANTS = [
   { id: 'mttrs_pt', domain: /(portugal|portugal\.origin)\.mtt\.rs/, language: 'pt' },
   { id: 'mttrs_us_es', domain: /(estadosunidos|estadosunidos\.origin)\.mtt\.rs/, language: 'es' },
   { id: 'mttrs_au', domain: /(australia|australia\.origin)\.mtt\.rs/, language: 'en' },
+  { id: 'mttrs_ca', domain: /(canada|canada\.origin)\.mtt\.rs/, language: 'en' },
+  { id: 'mttrs_de', domain: /(deutschland|deutschland\.origin)\.mtt\.rs/, language: 'de' },
+  { id: 'mttrs_es', domain: /(espana|espana\.origin)\.mtt\.rs/, language: 'es' },
+  { id: 'mttrs_uk', domain: /(unitedkingdom|unitedkingdom\.origin)\.mtt\.rs/, language: 'en' },
   { id: 'mttrs_us', domain: /(mtt|origin\.mtt)\.rs/, language: 'en' } // always in the end
 ]
